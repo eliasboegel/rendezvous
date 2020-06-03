0d4bfd66634491846fe851abaf80c3e3df7c9e3f
eliasboegel <62483075+eliasboegel@users.noreply.github.com>
2020-06-03 22:32:59 +0200

    Sanitized user input when running the game

--- rendezvous.py
+++ rendezvous.py
@@ -2,15 +2,32 @@ import os
 from rendezvous_class import Rendezvous
 
 # Read available missions from 'missions' subfolder
-missions_available = os.listdir('missions')
+try:
+    missions_available = os.listdir('missions')
+    
+    # Print list of available missions
+    print('Available missions: ')
+    for mission in missions_available:
+        print(mission.split('.')[0])
 
-# Print list of available missions
-print('Available missions: ')
-for mission in missions_available:
-    print(mission.split('.')[0])
+    # Ask user to select either random mission generation or a premade mission
+    while 1:
+        text = input("Please select mission to load or type 'r' to generate a random mission: ")
+        
+        text_with_extension = text + '.txt'
+        
+        if text == 'r':
+            selection = 'r'
+            break
+        
+        if text_with_extension in missions_available:
+            selection = text
+        else:
+            print('Selected mission not available, please try again.')
+    
+except FileNotFoundError:
+    selection = 'r'
 
-# Ask user to select either random mission generation or a premade mission
-selection = input("Please select mission to load or type 'r' to generate a random mission: ")
 
 # Run game with selected mission
 activemission = Rendezvous(selection)
